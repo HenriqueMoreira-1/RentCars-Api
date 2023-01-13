9e0aa6eea9cbe19cf56f6d03818495cdf1ed378c
Henrique Moreira <henriquemoreiradesouza4@gmail.com>
2023-01-13 15:30:28 -0300

    add lib dotenv

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,3 +1,4 @@
+import "dotenv/config"
 import express from "express"
 import "express-async-errors"
 import cors from "cors"
@@ -10,6 +11,6 @@ app.get("/", (request, response) => {
   return response.json({ message: "Olá dev!" })
 })
 
-app.listen(3000, () => {
-  console.log("Server started at port 3000 🏆")
+app.listen(process.env.PORT, () => {
+  console.log(`Server started at port ${process.env.PORT}! 🏆`)
 })
