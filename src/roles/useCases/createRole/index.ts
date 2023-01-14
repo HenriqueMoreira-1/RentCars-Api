11b3e4ab2552292d79fad94a3cd70de3a654776a
Henrique Moreira <henriquemoreiradesouza4@gmail.com>
2023-01-14 11:22:46 -0300

    config singleton in roles repository

--- src/roles/useCases/createRole/index.ts
+++ src/roles/useCases/createRole/index.ts
@@ -2,6 +2,6 @@ import { RolesRepository } from "@roles/repositories/rolesrepository"
 import { CreateRoleUseCase } from "./createRoleUseCase"
 import { CreateRoleController } from "./createRoleController"
 
-const rolesRepository = new RolesRepository()
+const rolesRepository = RolesRepository.getInstance()
 const createRoleUseCase = new CreateRoleUseCase(rolesRepository)
 export const createRolesController = new CreateRoleController(createRoleUseCase)
--- src/roles/useCases/listRoles/index.ts
+++ src/roles/useCases/listRoles/index.ts
@@ -2,6 +2,6 @@ import { RolesRepository } from "@roles/repositories/rolesrepository"
 import { ListRolesUseCase } from "./listRolesUseCase"
 import { ListRolesController } from "./listRolesController"
 
-const rolesRepository = new RolesRepository()
+const rolesRepository = RolesRepository.getInstance()
 const listRolesUseCase = new ListRolesUseCase(rolesRepository)
 export const listRolesController = new ListRolesController(listRolesUseCase)
