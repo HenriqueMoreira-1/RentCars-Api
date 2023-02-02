c177d6f5e02f3ed3a82fa332a62d0a29cba2d716
Henrique Moreira <henriquemoreiradesouza4@gmail.com>
2023-02-02 08:12:14 -0300

    Create cars migration

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -8,11 +8,19 @@ import { CreateSpecification1674673739060 } from "./migrations/1674673739060-Cre
 import { User } from "src/accounts/infra/typeorm/entities/User"
 import { AlterUserAddAvatar1675113522881 } from "./migrations/1675113522881-AlterUserAddAvatar"
 import { CreateUsers1675180591964 } from "./migrations/1675180591964-CreateUsers"
+import { CreateCars1675335365090 } from "./migrations/1675335365090-CreateCars"
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
   entities: [Role, Category, Specification, User],
-  migrations: [CreateRolesTable1673793794633, CreateCategory1674672942997, CreateSpecification1674673739060, CreateUsers1675180591964, AlterUserAddAvatar1675113522881],
+  migrations: [
+    CreateRolesTable1673793794633,
+    CreateCategory1674672942997,
+    CreateSpecification1674673739060,
+    CreateUsers1675180591964,
+    AlterUserAddAvatar1675113522881,
+    CreateCars1675335365090,
+  ],
   synchronize: true,
 })
