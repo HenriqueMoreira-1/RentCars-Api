0635700187e115ed1696ada2211a11629b18ad9b
Henrique Moreira <henriquemoreiradesouza4@gmail.com>
2023-02-04 11:24:30 -0300

    Add validation to create cars route with celebrate

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,8 +1,23 @@
 import { Router } from "express"
 import { CreateCarController } from "../../../../cars/useCases/createCar/CreateCarController"
+import { celebrate, Joi, Segments } from "celebrate"
 
 export const carsRoutes = Router()
 
 const createCarController = new CreateCarController()
 
-carsRoutes.post("/", createCarController.handle)
+carsRoutes.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      description: Joi.string().required(),
+      daily_rate: Joi.string().required(),
+      license_plate: Joi.string().required(),
+      fine_amount: Joi.number().positive().min(1).required(),
+      brand: Joi.string().required(),
+      category_id: Joi.string().uuid().required(),
+    }),
+  }),
+  createCarController.handle,
+)
