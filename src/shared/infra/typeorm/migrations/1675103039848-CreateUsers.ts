52ca72f70d3b469098d7692a420839ed64ece4f4
Henrique Moreira <henriquemoreiradesouza4@gmail.com>
2023-02-19 12:10:13 -0300

    fix(migrations): users primary key

--- src/shared/infra/typeorm/migrations/1675103039848-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1675103039848-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1675103039848 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
